cc03a3c051e268c7865b2c2b1f817a0dfbfd2fba
Marcin Sas-Szymanski <marcin.sas-szymanski@anixe.pl>
2021-02-18 14:59:58 +0100

    Add project

---
+++ NUnitStaticExpectTest/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using static NUnit.StaticExpect.Expectations;
+
+namespace NUnitStaticExpectTest
+{
+    class Tests
+    {
+        [Test]
+        public void Foo()
+        {
+            System.IO.File.Create("foo");
+            Expect(1, Is.EqualTo(1));
+        }
+        [Test]
+        public void Baz()
+        {
+            System.IO.File.Create("baz");
+        }
+    }
+}
